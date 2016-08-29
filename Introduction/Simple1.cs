e87cc8025366d282f738b45685481a12b85dc97f
Peter Oldhammer <peter.oldhammer@gmail.com>
2016-08-29 10:20:00 +0200

    Base Commit

---
+++ Introduction/Simple1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AdvancedCsharp.Intro.Introduction
+{
+    class Simple1
+    {
+        
+        public static void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Ange tal1: ");
+            var number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange tal2: ");
+            var number2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange tal3: ");
+            var number3 = int.Parse(Console.ReadLine());
+
+            var result = number1 + number2 + number3;
+
+            Console.WriteLine($"Summa av talen är {result}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace AdvancedCsharp.Intro
+{
+    using Introduction;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Simple1.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
